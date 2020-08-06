970b2a273dc401fb8347c3064e230bc693076162
tenus88 <diachenkoandrei1@gmail.com>
2020-08-05 20:29:56 -0400

    HeyHey

---
+++ src/utility/ReversAString.java
@@ -0,0 +1,7 @@
+package utility;
+
+public class ReversAString {
+    public static void main(String[] args) {
+        //ku-ku Grisha
+    }
+}
---
+++ src/utility/ZombieAttack.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class ZombieAttack {
+    public static void main(String[] args) {
+        String str = "No more zombie";
+        System.out.println(str);
+        System.out.println("12345");
+    }
+}
